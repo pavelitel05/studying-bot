6c361c1d2494f5ddddafb1b6e6e0e0b43fdc86d7
Pavel <pavel.stanislavovich.04@gmail.com>
2023-01-03 16:51:45 +0300

    Перенес конфигурацию телеграм бота в java код

--- src/main/java/com/example/tgbot/config/TelegramBotConfigurationProperties.java
+++ src/main/java/com/example/tgbot/config/TelegramBotConfigurationProperties.java
@@ -1,9 +1,17 @@
 package com.example.tgbot.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+@Getter
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "telegram-bot")
 public class TelegramBotConfigurationProperties {
+    private String token;
+    private String apiUrl;
+    private String name;
+    private String webhookPath;
 }
