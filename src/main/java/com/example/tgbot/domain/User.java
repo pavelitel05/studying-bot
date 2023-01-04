6f8563affbfbaa8e43304a5434327e9b8504efa0
Pavel <pavel.stanislavovich.04@gmail.com>
2023-01-04 13:22:32 +0300

    Сущность пользователя

--- src/main/java/com/example/tgbot/domain/User.java
+++ src/main/java/com/example/tgbot/domain/User.java
@@ -1,4 +1,28 @@
 package com.example.tgbot.domain;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "chatId", nullable = false)
+    private Long chatId;
+
+    @Column(name = "role")
+    private String role;
+
+    @Column(name = "module")
+    private String module;
 }
